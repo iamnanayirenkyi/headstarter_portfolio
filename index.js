49d5ba74f9b7c64ed3dfb71abeac60ea3090fb9a
andrews yirenkyi dwirah <andrewsnanayirenkyi@gmail.com>
2024-08-21 16:50:02 +0000

    Enhanced appearance

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ navBar = document.querySelector("nav");
 
 window.onscroll = function () {
   if (window.scrollY > 1) {
-    navBar.style.background = "#262f4a";
+    navBar.style.background = "#78c5e9";
   } else {
     navBar.style.background = "transparent";
   }
 };
-
-navText = document.getElementsByClassName("container");
