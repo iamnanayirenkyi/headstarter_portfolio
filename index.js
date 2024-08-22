10de309d02291128baed698553860b7a43a02f4e
andrews yirenkyi dwirah <andrewsnanayirenkyi@gmail.com>
2024-08-22 10:39:00 +0000

    update user interface

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ navBar = document.querySelector("nav");
 
 window.onscroll = function () {
   if (window.scrollY > 1) {
-    navBar.style.background = "#78c5e9";
+    navBar.style.background = "#fed0d7";
   } else {
     navBar.style.background = "transparent";
   }
